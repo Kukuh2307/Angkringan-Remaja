743008a3eebdc0cf1795124938315d55b44c25a4
Kukuh2307 <kukukhagung12@gmail.com>
2023-02-04 22:13:49 +0700

    (finish) hero section

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ hamburgerMenu.addEventListener('click', () => {
     navbarNav.classList.toggle('slide');
 });
 
-// click di luar side bar untuk menghilangkan navbar
+// ketika user mengeklik diluar icon navbar(tablet,mobile) maka hilangkan navbar samping
 document.addEventListener('click', function(e) {
     if(!hamburgerMenu.contains(e.target) && !navbarNav.contains(e.target)){
         navbarNav.classList.remove('slide');
